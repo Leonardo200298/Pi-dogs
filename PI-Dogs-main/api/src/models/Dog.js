eac8dff6ac3f2ae3e85c063793cc288737822639
Leonardo Sanchez <leonardosanchez119@gmail.com>
2022-03-12 16:38:37 -0300

    Feat: add database done to github

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -4,9 +4,27 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id:{
+      allowNull: false,
+      type:DataTypes.UUID,
+      primaryKey:true,
+      defaultValue: DataTypes.UUIDV4
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    weight:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    yearsOfLife:{
+      type: DataTypes.STRING,
+    }
   });
 };
