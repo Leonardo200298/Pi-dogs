c93d4c9902cc1c806bd852cac3e9550cc0625d58
Leonardo Sanchez <leonardosanchez119@gmail.com>
2022-03-14 15:59:09 -0300

    Feat add content to github, first success request

--- PI-Dogs-main/api/src/routes/dogs.js
+++ PI-Dogs-main/api/src/routes/dogs.js
@@ -4,13 +4,17 @@ const axios = require('axios')
 const API = 'https://api.thedogapi.com/v1/breeds?key=';
 const APIKEY = '350b7c31-14a9-4f82-ba04-d95e89453761'
 
-router.get('/', (req,res)=>{
-   /*  let dogData =await axios.get(API+APIKEY)
-    return res.send(dogData) */
-    let dogData = axios.get(API+APIKEY)
+router.get('/', async (req, res) => {
+    /*  let dogData =await axios.get(API+APIKEY)
+     return res.send(dogData) */
+
+
+    const dogData = await axios.get(API+APIKEY)
+    //console.log(JSON.stringify(dogData.data))
     Promise.all([dogData])
     .then((respuesta)=>{
-        return res.send(respuesta)
+        const [dataOfDoggies] = respuesta
+        return res.send(dataOfDoggies.data)
     })
 })
 
